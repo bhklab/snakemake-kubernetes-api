b94f38a641ce81d954e893a55fb648f9969ba2ce
Minoru Nakano <minoru.nakano@gmail.com>
2022-05-11 18:52:03 +0000

    added log file api routes

--- resources/logs.py
+++ resources/logs.py
@@ -1,13 +1,24 @@
-import traceback
+import os, traceback
 from flask_restful import Resource
-from flask import request
+from flask import request, send_file
+from decouple import config
 
 class ListLogs(Resource):
     def get(self):
         status = 200
-        response = {}
+        response = {
+            "logs": []
+        }
         try:
-            print('list logs')
+            path = None
+            pipeline = request.args.get('pipeline')
+            response['pipeline'] = pipeline
+            if pipeline is not None:
+                path = os.path.join(config('SNAKEMAKE_ROOT'), pipeline + '-snakemake', '.snakemake/log')
+            if path is not None and os.path.exists(path):
+                logs = os.listdir(path)
+                logs.sort(reverse=True)
+                response['logs'] = logs
         except Exception as e:
             print('Exception ', e)
             print(traceback.format_exc())
@@ -24,8 +35,19 @@ class DownloadLog(Resource):
     def get(self):
         status = 200
         response = {}
+        file_path = None
+        filename = None
         try:
-            print('get log')
+            pipeline = request.args.get('pipeline')
+            filename = request.args.get('filename')
+            if pipeline is not None and filename is not None:
+                path = os.path.join(config('SNAKEMAKE_ROOT'), pipeline + '-snakemake', '.snakemake/log', filename)
+                if os.path.isfile(path):
+                    file_path = path 
+                else:
+                    response['message'] = 'The requested log file does not exist.' 
+            else:
+                response['message'] = 'Missing pipeline name and/or log filename.'
         except Exception as e:
             print('Exception ', e)
             print(traceback.format_exc())
@@ -33,7 +55,10 @@ class DownloadLog(Resource):
             response['message'] = str(e)
             status = 500
         finally:
-            return response, status
+            if file_path is not None:
+                return send_file(file_path, as_attachment=True, attachment_filename=filename)
+            else:
+                return response, status
     
     def post(self):
         return 'Only get request is allowed', 400      
